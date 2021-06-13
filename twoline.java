fe808c5b848628d2b78762c4b18279e7992b16f2
DininduKavinda <68457066+DininduKavinda@users.noreply.github.com>
2021-06-13 17:38:19 +0530

    Add files via upload

---
+++ twoline.java
@@ -0,0 +1,7 @@
+class twoline{
+	public static void main(String[] Args){
+		System.out.println("Jenny is John’s sister.");
+		System.out.print("Jenny is a first year undergraduate student in Faculty of Management, University of Ohio");
+		
+	}
+}
